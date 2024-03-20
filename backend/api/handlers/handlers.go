9fe0c510806d5a2894af425ef50c2388affbfb01
Dot-P <metaalice01@gmail.com>
2024-03-20 12:31:53 +0900

    Refactor: Install gorilla/mux for improving the readness of code

--- backend/api/handlers/handlers.go
+++ backend/api/handlers/handlers.go
@@ -7,46 +7,22 @@ import (
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "hello, world!\n")
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "hello, world!\n")
 }
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Article...\n")
-	} else {
-		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Posting Article...\n")
 }
 func ArticleListHander(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Article List")
-	} else {
-		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Article List")
 }
 func ArticleDetailHander(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		articleID := 1
-		resString := fmt.Sprintf("Article No.%d", articleID)
-		io.WriteString(w, resString)
-	} else {
-		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
-	}
+	articleID := 1
+	resString := fmt.Sprintf("Article No.%d", articleID)
+	io.WriteString(w, resString)
 }
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Nice...")
-	} else {
-		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Posting Nice...")
 }
 func PostCommentHander(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Comment...")
-	} else {
-		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Posting Comment...")
 }
--- backend/api/main.go
+++ backend/api/main.go
@@ -4,18 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/sns/backend/handlers"
 )
 
 func main() {
+	r := mux.NewRouter()
 
-	http.HandleFunc("/hello", handlers.HelloHandler)
-	http.HandleFunc("/article", handlers.PostArticleHandler)
-	http.HandleFunc("/article/list", handlers.ArticleListHander)
-	http.HandleFunc("/article/1", handlers.ArticleDetailHander)
-	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
-	http.HandleFunc("/comment", handlers.PostCommentHander)
+	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", handlers.ArticleListHander).Methods(http.MethodGet)
+	r.HandleFunc("/article/1", handlers.ArticleDetailHander).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", handlers.PostCommentHander).Methods(http.MethodPost)
 
 	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
